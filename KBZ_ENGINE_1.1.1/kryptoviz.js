35021be3c11d1e10f78555cea75e0f9ba48c8171
NetCinematics <alpinefalcon@protonmail.com>
2022-01-25 20:50:20 -0800

    refactor modules

---
+++ KBZ_ENGINE_1.1.1/kryptoviz.js
@@ -0,0 +1,24 @@
+// /* KRYPTOBOOKZ ENGINE : KBZ
+//  * descriptions:
+//  * This engine looks up EPISODE IDTs
+//  * From BLOCKCHAIN.
+//  * Then converts IDX to IPFS URIs
+//  * The content from IPFS is then
+//  * LOAD and RENDER.
+//  * If IDT is array, display last.
+//  * LINKS displayed by 8GRID hash.
+//  * On LINK, lookup and RENDER.
+//  * Keep an ENGINE variable in series
+//  * switch render by KBZ:1.1.1
+    // * by spazeFalcon
+
+// import { viz } from "./KBZ_ENGINE_1.1.1/kryptoviz.js";
+
+let viz = {
+ name:44
+
+}
+
+export { viz };
+
+
